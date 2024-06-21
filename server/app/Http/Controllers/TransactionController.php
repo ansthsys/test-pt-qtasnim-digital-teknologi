2e606b2ee3d86e174923c8cfb3c65a4893cbc67f
ansthsys <azmiythufail91@gmail.com>
2024-06-22 01:05:25 +0700

    feat: Get All Transactions

--- server/app/Http/Controllers/TransactionController.php
+++ server/app/Http/Controllers/TransactionController.php
@@ -16,6 +16,42 @@ class TransactionController extends Controller
   {
   }
 
+  public function index(Request $request)
+  {
+    $msg = "GET all Transactions successfully";
+    $order = ['item_name', 'transaction_stock', 'transaction_quantity', 'transaction_date', 'type_name'];
+    $sort = ['asc', 'desc'];
+    $data = DB::table('transactions')
+      ->join('items', 'transactions.item_id', '=', 'items.id')
+      ->join('item_types', 'items.item_type_id', '=', 'item_types.id')
+      ->select([
+        'transactions.id as transaction_id',
+        'transactions.quantity as transaction_quantity',
+        'transactions.remaining_stock as transaction_stock',
+        'transactions.date as transaction_date',
+        'items.id as item_id',
+        'items.name as item_name',
+        // 'items.stock as item_stock',
+        'item_types.id as type_id',
+        'item_types.name as type_name'
+      ]);
+
+    if ($request->has('search') && $request->search != '') {
+      $query = strtolower($request->search);
+      $data->where(DB::raw('LOWER(items.name)'), 'like', "%$query%");
+    }
+
+    if ($request->has('order') && in_array($request->order, $order)) {
+      $orderBy = $request->order;
+      $sortBy = $request->has('sort') && in_array($request->sort, $sort) ? $request->sort : 'asc';
+      $data->orderBy($orderBy, $sortBy);
+    }
+
+    $data = $data->get();
+
+    return $this->successResponse($msg, $data);
+  }
+
   public function store(Request $request)
   {
     $this->validate($request, [
